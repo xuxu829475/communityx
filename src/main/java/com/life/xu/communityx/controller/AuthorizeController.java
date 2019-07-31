794f66ba3c31964e81ab8872bcf940db58c8ff90
xuxu <xu829475@qq.com>
2019-07-31 17:14:58 +0800

    登录

--- src/main/java/com/life/xu/communityx/controller/AuthorizeController.java
+++ src/main/java/com/life/xu/communityx/controller/AuthorizeController.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * @program: communityx
  * @description: github授权登录
@@ -31,7 +35,10 @@ public class AuthorizeController {
 
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletResponse response,
+                           HttpServletRequest request,
+                           HttpSession session){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -40,6 +47,12 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = gitHubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            //登录成功，写cookie 和session
+            session.setAttribute("user",githubUser);
+        }else {
+            //登录失败，重新登录
+        }
         return "index";
     }
 }
